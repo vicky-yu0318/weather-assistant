17b102659744ccd53a1b7d5e882384a7ce4dc237
vicky-yu <freedom0318v@gmail.com>
2024-02-06 23:29:42 +0800

    add: output in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: false,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      // Add other routes here
+    };
+  },
 };
 
 export default nextConfig;
