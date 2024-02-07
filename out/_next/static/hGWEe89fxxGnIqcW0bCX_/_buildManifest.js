8cd428507421cb41ec9624106561c6ea5056080e
vicky-yu <freedom0318v@gmail.com>
2024-02-07 12:07:29 +0800

    publis

---
+++ out/_next/static/hGWEe89fxxGnIqcW0bCX_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/29107295-2a9ca7f07685bf37.js","static/chunks/90-1d6c6a9074a7d301.js","static/chunks/pages/index-1c1a02dabf144317.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
